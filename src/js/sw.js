9eea925235278f61c9fbab6dc3b329c1628a9a8c
RedstoneBlocks <58533999+RedstoneBlocks@users.noreply.github.com>
2020-10-29 10:08:05 -0500

    add events to sw.js

--- src/js/sw.js
+++ src/js/sw.js
@@ -1,1 +1,48 @@
+var CACHE_NAME = 'cache-v1';
+var urlsToCache = [
+  '/',
+  '/css/main.css',
+  '/js/main.js'
+];
 
+self.addEventListener('install', function(event) { //install script
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) { //fetching files
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
+
+self.addEventListener('activate', function(event) {
+
+  var cacheAllowlist = ['cache-v1'];
+
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
